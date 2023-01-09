0be0c08d6d9edf15e0ac1dd6106db9882dfc1fd3
hamzaj1127 <hamzaj1127@gmail.com>
2023-01-09 09:33:04 -0500

    part 2

--- Pages/NewsPage.xaml.cs
+++ Pages/NewsPage.xaml.cs
@@ -1,9 +1,26 @@
 namespace NewsApp.Pages;
+using NewsApp.Services;
+using NewsApp.Models;
 
 public partial class NewsPage : ContentPage
 {
+
+	public List<Article> ArticlesList { get; set; }
 	public NewsPage()
 	{
 		InitializeComponent();
+		ArticlesList = new List<Article>();
+	}
+
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+		ApiService apiService = new ApiService();
+		var newsResult = await apiService.GetNews();
+		foreach (var item in newsResult.Articles)
+		{
+			ArticlesList.Add(item);
+		}
+		CvNews.ItemsSource = ArticlesList;
 	}
 }
---
+++ Services/ApiService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NewsApp.Models;
+
+namespace NewsApp.Services
+{
+    public class ApiService
+    {
+        public async Task<Root> GetNews()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync("https://gnews.io/api/v4/top-headlines?token=3f9dc2ef595ff2b3cc4e6bc611f6841a&lang=en&topic=sports");
+
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+    }
+}
